ed33aeab2a5069c6630944574c14e9bb9f9a7329
1972dream <ming74.kim@gmail.com>
2022-12-27 20:15:55 +0900

    컨디션

---
+++ DAY2/condition.py
@@ -0,0 +1,65 @@
+# if 예제
+# apple = 10
+# orange = 20
+# if apple > orange:
+#     print("사과가 큼")
+# else:
+#     print("오랜지 큼")
+
+# math = 100
+# english = 100
+# if math > english:
+#     print("수확을 더 잘함")
+# elif math < english:
+#     print("영어를 더 잘함")
+# else:
+#     print("영어 수확 비슷")
+
+
+# for 예제
+# for i in range(3):
+#     print("안녕")
+# for i in range(10):
+#     print(i + 1)
+# for i in range(1, 11):
+#     print(i)
+# for i in range(10):
+#     if i % 2 == 1:
+#         print(i)
+# for i in range(11):
+#     if i % 2 == 0:
+#         print(i)
+# sum = 0
+# for i in range(10, 21):
+#     if i % 3 == 0:
+#         sum += i
+# print(sum)
+# sum = 0
+# suii = 0
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         sum += i
+#     elif i % 2 == 1:
+#         suii += i
+# print(sum - suii)
+
+# for i in range(50, 81):
+#     if i % 3 == 0:
+#         if i % 4 == 0:
+#             print(i)
+
+# sum = 1
+# for i in range(1, 21):
+#     if i % 5 == 0:
+#         sum *= i
+# print(sum)
+
+
+# python
+# for 3 가지
+# 1 숫자1
+#     for i in range(10):
+# 2 범위(숫자2)
+#     for i in range(0,10):
+# 3 범위,증감(숫자3)
+#     for i in range(0,10,1):
