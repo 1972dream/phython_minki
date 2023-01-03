ae6eff7e2c9cbd7db9e27b24f41a3bf5431a2314
1972dream <ming74.kim@gmail.com>
2023-01-03 20:46:01 +0900

    조건문222222222222222222222222222222222222

---
+++ day3/condition2.py
@@ -0,0 +1,72 @@
+# while 조건:
+# 조건이 참일떄 실행하는 코드
+# 무한 반복 하고 싶을떄
+
+# while true:
+#     print("hi")
+
+# while True:
+#     print("hi")
+
+# while False:
+#     print("hello")
+
+# i = 0
+# while i < 3:
+#     print("hi")
+#     i += 1
+
+# i = 0
+# while i < 100:
+#     print("오늘 화요일임 ㅅㄱ")
+#     i += 1
+
+# i = 1
+# while i < 11:
+#     print(i)
+#     i += 1
+
+1
+# i = 1
+# while i < 11:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+2
+# i = 1
+# while i < 11:
+#     if i % 2 != 0:
+#         print(i)
+#     i += 1
+3
+# i = 1
+# sum = 1
+# while i < 100:
+#     i += 1
+#     sum += i
+# print(sum)
+4
+# i = 1
+# while i < 100:
+#     if i % 2 == 0 and i % 3 == 0 and i % 5 == 0:
+#         print(i)
+#     i += 1
+5
+# i = 1
+# sum = 1
+# while i < 20:
+#     if i % 7 == 0:
+#         sum *= i
+#     i += 1
+# print(sum)
+6
+# i = 1
+# sum = 1
+# suii = 1
+# while i < 100:
+#     if i % 2 == 0:
+#         sum += i
+#     if i % 2 != 0:
+#         suii += i
+#     i += 1
+# print(suii - sum)
