cb442104626af0060f60535852c32413b4f7e043
1972dream <ming74.kim@gmail.com>
2023-03-07 21:04:29 +0900

    list

--- day6/list.py
+++ day6/list.py
@@ -31,7 +31,100 @@ ex) a=[]
 sort()
 reverse()
 =내림차순
+<특8>
+리스트와 리스트를 합칠때 + 사용
+메소드는 extend
+<특9>
+리스트를 비울때 clear 메소드 사용
+<특10>
+특정 위치에 값을 추가 할때 insert 메소드 사용
+<특11>
+값이 몇번째 있는데 찾을떄 index 메소드 사용
+<특12>
+리스트안에 값이 몇개 있는지 알아볼때 count 메소드 사용
+<특13>
+리스트의 길이를 알고싶을때 len 함수 사용
+<특14>
+ a = ["바나나", "사과", "오렌지"]
+for i in range(len(a)):
+    print(a[i])
+
+a = ["바나나", "사과", "오렌지"]
+for i in a:
+    print(i)
 """
+
+# q2
+# fruits = ["orange", "cherry", "watermelon", "apple", "blueberry"]
+# fruits.sort()
+# fruits.reverse()
+
+# for i in range(len(fruits)):
+#     print(fruits[i])
+# q2
+# phonenum = [0, 1, 0, 1, 2, 3, 4, 5, 6, 7]
+# phonenum.clear()
+# phonenum.append(0)
+# phonenum.append(1)
+# phonenum.append(0)
+# phonenum.append(9)
+# phonenum.append(8)
+# phonenum.append(7)
+# phonenum.append(6)
+# phonenum.append(5)
+# phonenum.append(4)
+# phonenum.append(3)
+# print(phonenum)
+# q3
+# 민기 = []
+# for i in range(1, 21):
+#     if i % 3 == 0:
+#         민기.append(i)
+# 민기.reverse()
+# print(민기)
+# q4
+def 와샌즈(x, y):
+    민기 = []
+    for i in range(1, 7):
+        min(x, y)
+
+
+# a = ["바나나", "사과", "오렌지"]
+# for i in range(len(a)):
+#     print(a[i])
+
+# a = ["바나나", "사과", "오렌지"]
+# for i in a:
+#     print(i)
+
+
+# h = [1, 2, 3, 4]
+# print(len(h))
+
+
+# g = [1, 2, 3, 4, 2]
+# print(g.count(5))
+
+
+# f = [1, 2, 3, 4, 2]
+# print(f.index(2))
+
+
+# e = [1, 2, 3]
+# e.insert(1, 100)
+# print(e)
+# a = [1, 2]
+# b = [3, 4]
+# c = a + b
+# print(c)
+# a = [1, 2]
+# b = [3, 4]
+# a.extend(b)
+# print(a)
+
+# d = [100, 200]
+# d.clear()
+# print(d)
 # a = 10
 # b = "민기"
 # c = ["사과", "귤", "바나나"]
@@ -116,15 +209,20 @@ reverse()
 
 # 구구단(7)
 # q3 짝수(3)
-def 짝수(num):
-    민기 = []
-    for i in range(1, num * 2 + 1):
-        if i % 2 == 0:
-            민기.append(i)
-    민기.reverse()
-    print(민기)
+# def 짝수(num):
+#     민기 = []
+#     for i in range(1, num * 2 + 1):
+#         if i % 2 == 0:
+#             민기.append(i)
+#     민기.reverse()
+#     print(민기)
 
 
-짝수(3)
-짝수(6)
-짝수(4)
+# 짝수(3)
+# 짝수(6)
+# 짝수(4)
+
+# a = [2, 4, 1, 3]
+# a.sort()
+# a.reverse()
+# print(a)
